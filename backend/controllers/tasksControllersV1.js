a91fb66d86335a0c00a66bec8cac8c5f81302635
David Ku <hola@davidku.dev>
2022-03-10 22:47:21 -0500

    se añade la logica

--- backend/controllers/tasksControllersV1.js
+++ backend/controllers/tasksControllersV1.js
@@ -1,21 +1,62 @@
-const getAllTasks = (req, res) => {
-  res.status(200).json({ message: "obtener todas las tarea" });
+const asyncHandler = require("express-async-handler");
+
+const tasks = require("../models/tasksModelsV1");
+
+const getAllTasks = async (req, res) => {
+  const task = await tasks.find();
+  res.status(200).json(task);
 };
 
-const getTask = (req, res) => {
+const getTask = async (req, res) => {
   res.status(200).json({ message: "obtener una tarea" });
 };
 
-const createTask = (req, res) => {
-  res.status(200).json({ message: "Crear una tarea" });
+const createTask = asyncHandler(async (req, res) => {
+  if (!req.body.title) {
+    // res.status(400).json({ message: "Titulo requerido" })
+    res.status(400);
+    throw new Error("Titulo requerido");
+  }
+
+  const newTask = await tasks.create({
+    title: req.body.title,
+    description: req.body.description,
+  });
+
+  res.status(200).json(newTask);
+});
+
+const updateTask = async (req, res) => {
+  // const search = tasks.findById(req.params.id);
+
+  // if (!search) {
+  //   res.status(400);
+  //   throw new Error(`No encontre una tarea con el ID: ${req.params.id}`);
+  // }
+
+  const updateTask = await tasks.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  if (updateTask === null) {
+    res.status(400);
+    throw new Error("No encontre la tarea");
+  }
+
+  res.status(200).json(updateTask);
 };
 
-const updateTask = (req, res) => {
-  res.status(200).json({ message: "actualizar una tarea" });
-};
+const deleteTask = async (req, res) => {
+  const search = tasks.findById(req.params.id);
 
-const deleteTask = (req, res) => {
-  res.status(200).json({ message: "eliminar una tarea" });
+  if (!search) {
+    res.status(400);
+    throw new Error(`No encontre una tarea con el ID: ${req.params.id}`);
+  }
+
+  await search.remove();
+
+  res.status(200).json({ id: req.params.id });
 };
 
 module.exports = {
