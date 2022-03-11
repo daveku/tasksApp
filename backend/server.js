d8ac41f59a34e7617c8a92551f4f66a3f5f615cd
David Ku <hola@davidku.dev>
2022-03-10 22:47:11 -0500

    se coloca los componentes en server.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/v1/tasks", require("./routes/tasksRoutesV1"));
 
+app.use(errorHandler);
 app.listen(port, () => console.log(`El servidor inicio en el puerto: ${port}`));
