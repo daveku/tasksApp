e08b318e16be792629da39e76bde0744d5e6ebc2
David Ku <hola@davidku.dev>
2022-03-16 16:50:59 -0500

    se añade el protect middleware a router de tareas

--- backend/routes/tasksRoutesV1.js
+++ backend/routes/tasksRoutesV1.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/tasksControllersV1");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").get(controller.getAllTasks).post(controller.createTask);
+router
+  .route("/")
+  .get(protect, controller.getAllTasks)
+  .post(protect, controller.createTask);
 
 // router.get("/", controller.getAllTasks);
 // router.post("/", controller.createTask);
 
 router
   .route("/:id")
-  .get(controller.getTask)
-  .put(controller.updateTask)
-  .delete(controller.deleteTask);
+  .get(protect, controller.getTask)
+  .put(protect, controller.updateTask)
+  .delete(protect, controller.deleteTask);
 
 // router.get("/:id", controller.getTask);
 // router.put("/:id", controller.updateTask);
