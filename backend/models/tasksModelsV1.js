397fcf5c3cd1a96cadf587e62abfc82690161a57
David Ku <hola@davidku.dev>
2022-03-16 16:51:20 -0500

    se añade usuario al modelo de tareas.

--- backend/models/tasksModelsV1.js
+++ backend/models/tasksModelsV1.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose");
 
 const taskSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     title: {
       type: String,
       required: [true, "Titulo requerido"],
